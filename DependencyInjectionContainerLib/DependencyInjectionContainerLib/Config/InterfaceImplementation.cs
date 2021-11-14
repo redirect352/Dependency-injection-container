4222f86d25d7c1870fd7b8bb560f94777a993cb5
Ponomarev Nikita <79397499+redirect352@users.noreply.github.com>
2021-11-14 18:00:09 +0300

    Library structure

---
+++ DependencyInjectionContainerLib/DependencyInjectionContainerLib/Config/InterfaceImplementation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainerLib.Config
+{
+    class InterfaceImplementation
+    {
+        public Type Type { get; private set; }
+        public ImplementationsTTL TTL { get; private set }
+
+        public InterfaceImplementation(Type type, ImplementationsTTL tTL)
+        {
+            TTL = tTL;
+            Type = type;
+        }
+    }
+}
